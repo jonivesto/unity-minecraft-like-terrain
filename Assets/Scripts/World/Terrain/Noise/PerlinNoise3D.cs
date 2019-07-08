9555293d645d2b96f5ead01c3b27351666dab551
jonivesto <n7vejo00@students.oamk.fi>
2019-07-09 01:09:22 +0300

    Perlin 3D, Ore textures

---
+++ Assets/Scripts/World/Terrain/Noise/PerlinNoise3D.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class PerlinNoise3D
+{
+    public static float Evaluate(float x, float y, float z)
+    {
+        float AB = Mathf.PerlinNoise(x,y);
+        float BC = Mathf.PerlinNoise(y, z);
+        float AC = Mathf.PerlinNoise(x, z);
+
+        float BA = Mathf.PerlinNoise(y, x);
+        float CB = Mathf.PerlinNoise(z, y);
+        float CA = Mathf.PerlinNoise(z, x);
+
+        float ABC = AB + BC + AC + BA + CB + CA;
+        return ABC / 6;
+    }
+}
